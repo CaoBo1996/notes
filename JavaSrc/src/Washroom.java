3025565ed6032248c2d69d5d90060836c6a5ef11
caobo <994343079@qq.com>
2019-11-07 14:09:59 +0800

    add producer and consumer

---
+++ JavaSrc/src/Washroom.java
@@ -0,0 +1,16 @@
+public class Washroom{
+    private volatile boolean isAvailable = false;
+    private Object lock = new Object();
+
+    public boolean isAvailable(){
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available){
+        this.isAvailable = available;
+    }
+
+    public Object getLock(){
+        return lock;
+    }
+}
